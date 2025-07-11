6acde691f9bd66d6e1a089dbcdf8594c45642262
baal86 <43853528+baal86@users.noreply.github.com>
2025-07-11 17:41:37 +0200

    Added aiomqtt for discovery and polling

--- miner2mqtt/discovery.py
+++ miner2mqtt/discovery.py
@@ -2,6 +2,7 @@ import asyncio
 from pyasic import get_miner
 from pyasic.network import MinerNetwork
 import logging
+import aiomqtt
 import json
 
 miners = []
@@ -11,7 +12,7 @@ async def scan_miners(subnet):
     miners = await network.scan()
     return miners
     
-async def publish_discovery(mqttc,miner,prefix):
+async def publish_discovery(client,miner,prefix):
     mac = await miner.get_mac()
     mfr = miner.make
     model = await miner.get_model()
@@ -57,15 +58,22 @@ async def publish_discovery(mqttc,miner,prefix):
         "unit_of_measurement": "W",
     }
 
-    mqttc.publish("homeassistant/switch/{}/config".format(uuid + "_enable"),"{}".format(json.dumps(common_dict | enable_dict)),qos=1,retain=True)
-    mqttc.publish("homeassistant/sensor/{}/config".format(uuid + "_hashrate"),"{}".format(json.dumps(common_dict | hashrate_dict)),qos=1,retain=True)
-    mqttc.publish("homeassistant/sensor/{}/config".format(uuid + "_wattage"),"{}".format(json.dumps(common_dict | wattage_dict)),qos=1,retain=True)
+    await client.publish("homeassistant/switch/{}/config".format(uuid + "_enable"),payload="{}".format(json.dumps(common_dict | enable_dict)),qos=1,retain=True)
+    await client.publish("homeassistant/sensor/{}/config".format(uuid + "_hashrate"),payload="{}".format(json.dumps(common_dict | hashrate_dict)),qos=1,retain=True)
+    await client.publish("homeassistant/sensor/{}/config".format(uuid + "_wattage"),payload="{}".format(json.dumps(common_dict | wattage_dict)),qos=1,retain=True)
 
-async def task(procdata,subnet):
+async def task(procdata):
     while 1:
         logging.info("Discovering")
         async with procdata["lock"]:
-            procdata["miners"] = await scan_miners(subnet)
-            for miner in procdata["miners"]:
-                await publish_discovery(procdata["mqtt"],miner,procdata["prefix"])
+            procdata["miners"] = await scan_miners(procdata["subnet"])
+            async with aiomqtt.Client(
+                            hostname = procdata["server"],
+                            port = procdata["port"],
+                            username = procdata["user"],
+                            password = procdata["password"],
+                            clean_session=True
+                            ) as client:  
+                for miner in procdata["miners"]:
+                    await publish_discovery(client,miner,procdata["prefix"])
         await asyncio.sleep(30)
